8a150df3058d62c1027efc3c48c2b319516892ba
odoncore <ws7074@naver.com>
2017-06-07 23:18:46 +0900

    ros

---
+++ catkin_ws/build/ros_drone/cmake/ros_drone-genmsg-context.py
@@ -0,0 +1,11 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = "/home/wonsik/catkin_ws/src/ros_drone/msg/gps.msg"
+services_str = "/home/wonsik/catkin_ws/src/ros_drone/srv/gps_service.srv"
+pkg_name = "ros_drone"
+dependencies_str = "std_msgs"
+langs = "gencpp;genlisp;genpy"
+dep_include_paths_str = "ros_drone;/home/wonsik/catkin_ws/src/ros_drone/msg;std_msgs;/opt/ros/indigo/share/std_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "/usr/bin/python"
+package_has_static_sources = '' == 'TRUE'
+genmsg_check_deps_script = "/opt/ros/indigo/share/genmsg/cmake/../../../lib/genmsg/genmsg_check_deps.py"
