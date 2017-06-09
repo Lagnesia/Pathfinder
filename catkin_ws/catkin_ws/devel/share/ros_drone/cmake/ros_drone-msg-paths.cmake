965915e2da7270372e833c23780b6d183a29861d
odoncore <ws7074@naver.com>
2017-06-09 22:49:09 +0900

    last pub-sub failed

---
+++ catkin_ws/catkin_ws/devel/share/ros_drone/cmake/ros_drone-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(ros_drone_MSG_INCLUDE_DIRS "/home/wonsik/catkin_ws/src/ros_drone/msg")
+set(ros_drone_MSG_DEPENDENCIES std_msgs)
